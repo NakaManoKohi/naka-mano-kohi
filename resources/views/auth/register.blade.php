8fd72fba7d338df1b3a80b528321824da76f81d9
ekanata14 <ekanata1411@gmail.com>
2022-10-03 23:03:47 +0800

    Continuing login page and make register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,24 +14,32 @@
 	</div>
 </div>
 <div class="h-100 d-flex align-items-center justify-content-center">
-	<div class="row w-100 d-flex justify-content-center" style="height: 500px">
+	<div class="row w-100 d-flex justify-content-center" style="height: 650px">
 		<div class="form-container d-flex flex-row w-75">
 			<div class="img-column col-5 bg-brown h-100 d-flex justify-content-center align-items-center" style="box-shadow:10px 10px rgba(0,0,0,0.3)">
 				<img class="mb-4 w-50" src="/images/Naka_Mano_Kohi_yellow.png" alt="kohi-logo">
 			</div>
 			<div class="form-column col-7 p-0 bg-yellow h-100 d-flex justify-content-center align-items-center" style="box-shadow: 10px 10px rgba(0,0,0,0.3)">
 				<form class="w-75">
-					<h1 class="h3 mb-3 fw-bold">Masuk Kohiverse</h1>
+					<h1 class="h3 mb-3 fw-bold">Buat Akun</h1>
 					<div class="form-floating">
 						<input type="email" class="form-control mb-4 rounded-0 border-0" id="floatingInput" placeholder="name@example.com">
-						<label for="floatingInput">Email address</label>
+						<label for="floatingInput">Name</label>
+					</div>
+					<div class="form-floating">
+						<input type="email" class="form-control mb-4 rounded-0 border-0" id="floatingInput" placeholder="name@example.com">
+						<label for="floatingInput">Username</label>
+					</div>
+					<div class="form-floating">
+						<input type="email" class="form-control mb-4 rounded-0 border-0" id="floatingInput" placeholder="name@example.com">
+						<label for="floatingInput">Email</label>
 					</div>
 					<div class="form-floating">
 						<input type="password" class="form-control mb-4 rounded-0 border-0" id="floatingPassword" placeholder="Password">
 						<label for="floatingPassword">Password</label>
 					</div>
 					<button class="w-50 btn btn btn-brown rounded-0 mb-3 text-white fs-6" type="submit">Shot</button>
-					<p class="fs-5">Buat Akun Kopi Mu! <a href="/register" class="text-primary text-decoration-none">Shot It</a></p>
+					<p class="fs-5">Akun nya udah ada? <a href="/login" class="text-primary text-decoration-none">Tamping</a></p>
 					{{-- Make your own button effect --}}
 					<p class="text-muted">&copy; Naka Mano Kohi 2022</p>
 				</form>
--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/login', function(){
     return view('auth.index');
+});
+
+Route::get('/register', function(){
+    return view('auth.register');
 });
