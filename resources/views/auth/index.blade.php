4c6f396ab75b4539c94633c979e1fa9aab86bc8f
ekanata14 <ekanata1411@gmail.com>
2022-10-03 15:26:38 +0800

    Working on Auth Login View

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,98 +1,92 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.101.0">
-    <title>Signin Template · Bootstrap v5.2</title>
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
+@extends('auth.templates.main')
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
+@section('container')
+		{{-- <div class="row h-100 w-100 position-absolute d-flex justify-content-center" style="z-index:-99">
+	<div class="p-0 bg-primary">
+<div class="col-lg-12 d-flex justify-content-center align-items-center bg-brown" style="box-shadow: 10px 10px #000; height: 387px">
+			<img class="mb-4 w-50" src="/images/Naka_Mano_Kohi_yellow.png" alt="kohi-logo">
+		</div
+	</div>
+	<div class="col-7 p-0 bg-secondary">
+ <div class="col-lg-12 bg-yellow d-flex justify-content-center align-items-center" style="box-shadow: 0 10px #000">
+			<form class="w-50">
+				<h1 class="h3 mb-3 fw-bold">Masuk Kohiverse</h1>
+				<div class="form-floating">
+					<input type="email" class="form-control mb-4 rounded-0 border-0" id="floatingInput" placeholder="name@example.com">
+					<label for="floatingInput">Email address</label>
+				</div>
+				<div class="form-floating">
+					<input type="password" class="form-control mb-4 rounded-0 border-0" id="floatingPassword" placeholder="Password">
+					<label for="floatingPassword">Password</label>
+				</div>
+				<button class="w-50 btn btn-lg btn-brown rounded-0 mb-3 text-white fs-5" type="submit">Shot</button>
+				<p class="fs-5">Buat Akun Kopi Mu! <span class="text-primary">Shot It</span></p>
+				Make your own button effect
+				<p class="text-muted">&copy; Naka Mano Kohi 2022</p>
+			</form>
+		</div>
+	</div>
+</div>
+ <div class="form-container d-flex align-items-center justify-content-center w-100 h-100">
+	<div class="d-flex align-items-center form-container flex-row" style="height: 400px">
+		<div class="bg-primary h-100">
+			<img class="mb-4 w-50" src="/images/Naka_Mano_Kohi_yellow.png" alt="kohi-logo">
+		</div>
+		<div class="bg-info w-100 h-100 d-flex justify-content-center">
+			<form class="w-75">
+				<h1 class="h3 mb-3 fw-bold">Masuk Kohiverse</h1>
+				<div class="form-floating">
+					<input type="email" class="form-control mb-4 rounded-0 border-0" id="floatingInput" placeholder="name@example.com">
+					<label for="floatingInput">Email address</label>
+				</div>
+				<div class="form-floating">
+					<input type="password" class="form-control mb-4 rounded-0 border-0" id="floatingPassword" placeholder="Password">
+					<label for="floatingPassword">Password</label>
+				</div>
+				<button class="w-50 btn btn-lg btn-brown rounded-0 mb-3 text-white fs-5" type="submit">Shot</button>
+				<p class="fs-5">Buat Akun Kopi Mu! <span class="text-primary">Shot It</span></p>
+				Make your own button effect
+				<p class="text-muted">&copy; Naka Mano Kohi 2022</p>
+			</form>
+		</div>
+	</div>
+</div> --}}
+<div class="h-100 w-100 d-flex align-items-center justify-content-center position-absolute" style="z-index:-99">
+	<div class="row h-100 w-100">
+		{{-- <div class="form-container" style="400px"> --}}
+			<div class="col-5 p-0 bg-brown h-100 d-flex justify-content-center align-items-center">\
 
-      .b-example-divider {
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
+			</div>
+			<div class="col-7 p-0 bg-secondary h-100 d-flex justify-content-center align-items-center">
 
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
-
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
-
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
-
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="css/auth.css" rel="stylesheet">
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    
-  </head>
-  <body class="text-center">
-    
-<main class="form-signin w-100 m-auto">
-  <form>
-    <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-  </form>
-</main>
-
-
-    
-  </body>
-</html>
+			</div>
+		{{-- </div> --}}
+	</div>
+</div>
+<div class="h-100 d-flex align-items-center justify-content-center">
+	<div class="row w-100 d-flex justify-content-center" style="height: 400px">
+		<div class="form-container d-flex flex-row" style="width:700px">
+			<div class="col-5 bg-brown h-100 d-flex justify-content-center align-items-center" style="margin-right: -2px; box-shadow:10px 10px #000">
+				<img class="mb-4 w-50" src="/images/Naka_Mano_Kohi_yellow.png" alt="kohi-logo">
+			</div>
+			<div class="col-7 p-0 bg-yellow h-100 d-flex justify-content-center align-items-center" style="box-shadow: 10px 10px #000">
+				<form class="w-75">
+					<h1 class="h3 mb-3 fw-bold">Masuk Kohiverse</h1>
+					<div class="form-floating">
+						<input type="email" class="form-control mb-4 rounded-0 border-0" id="floatingInput" placeholder="name@example.com">
+						<label for="floatingInput">Email address</label>
+					</div>
+					<div class="form-floating">
+						<input type="password" class="form-control mb-4 rounded-0 border-0" id="floatingPassword" placeholder="Password">
+						<label for="floatingPassword">Password</label>
+					</div>
+					<button class="w-50 btn btn btn-brown rounded-0 mb-3 text-white fs-6" type="submit">Shot</button>
+					<p class="fs-5">Buat Akun Kopi Mu! <span class="text-primary">Shot It</span></p>
+					Make your own button effect
+					<p class="text-muted">&copy; Naka Mano Kohi 2022</p>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
