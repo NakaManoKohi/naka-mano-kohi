9d2e20576507399f2ccf2cfc36fb3da39d4360ae
ExRyze <vaisyagovinandas@gmail.com>
2022-10-05 10:55:55 +0800

    Update - Page Home - Initial - Main & Aside

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -2,7 +2,7 @@
   <div class="container-fluid d-flex justify-content-between align-items-center text-white">
     <a href="#" class="text-white d-flex align-items-center text-decoration-none">
       <img src="images/Naka_Mano_Kohi_yellow.png" alt="logo" width="60" class="me-4">
-      <h4 class="h4 m-0 text-yellow">Naka Mano Kohi</h4>
+      <h6 class="h6 m-0 text-yellow">Naka Mano Kohi</h6>
     </a>
     <button class="navbar-toggler bg-white color-edge" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -10,7 +10,7 @@
     <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a href="#" class="nav-link text-white text-decoration-none">Papoy, LilGru
+          <a href="#" class="nav-link text-white text-decoration-none">Papoy, Lil Gru
             <img src="images/lilgru.jpg" alt="profile" width="44" class="me-4 p-1 rounded-circle">
           </a>
         </li>
