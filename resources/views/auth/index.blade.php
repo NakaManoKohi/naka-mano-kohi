981e8e6935ad9c6d8d96d472577674ae52d3dcfd
ExRyze <vaisyagovinandas@gmail.com>
2022-10-12 11:21:31 +0800

    Update - Page Register - UI

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,23 +1,14 @@
 @extends('auth.templates.main')
 
 @section('container')
-<div class="d-flex h-100 col-12 position-absolute" style="z-index:-99">
-	<div class="h-100 bg-brown bg-form-margin"></div>
-	<div class="col-8">
-		<div class="h-100 bg-brown col-5"></div>
-	</div>
-</div>
-<div class="coffee-img position-absolute coffee-img-top"></div>
-<div class="coffee-img position-absolute coffee-img-bottom"></div>
-
 	<div class="w-100 d-flex align-items-center h-100">
-		<div class="d-flex align-items-stretch col-8 container-form">
+		<div class="d-flex align-items-stretch col-9 container-form">
 			<div class="img-column col-md-5 bg-brown d-flex justify-content-center align-items-center auth-box-shadow">
 				<img class="mb-4 w-50" src="/images/Naka_Mano_Kohi_yellow.png" alt="kohi-logo">
 			</div>
-			<div class="form-column col-md-7 p-0 bg-yellow d-flex justify-content-center align-items-center auth-box-shadow">
+			<div class="form-column col-md-7 p-3 bg-yellow d-flex justify-content-center align-items-center auth-box-shadow">
 				<form class="w-75" action="/login" method="post">
-					<h1 class="h3 mb-3 fw-bold">Masuk Kohiverse</h1>
+					<h1 class="mb-3 fw-bold">Masuk Kohiverse</h1>
 					@if(session()->has('success'))
 					<div class="alert alert-success alert-dismissible fade show" role="alert">
 						{{ session('success') }}
@@ -49,7 +40,7 @@
 					<button class="w-50 button button-brown mb-3 fs-5" type="submit">Shot</button>
 					<p class="fs-5">Buat Akun Kopi Mu! <a href="/register" class="text-primary text-decoration-none">Shot It</a></p>
 					{{-- Make your own button effect --}}
-					<p class="text-muted">&copy; Naka Mano Kohi 2022</p>
+					<p class="text-muted m-0">&copy; Naka Mano Kohi 2022</p>
 				</form>
 			</div>
 		</div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,28 +1,15 @@
 @extends('auth.templates.main')
 
 @section('container')
-<div class="h-100 w-100 d-flex align-items-center justify-content-center position-absolute" style="z-index:-99">
-	<div class="row h-100 w-100">
-		{{-- <div class="form-container" style="400px"> --}}
-			<div class="left-side-decoration col-3 p-0 bg-brown h-100 d-flex justify-content-center align-items-center" style="box-shadow:0 10px rgba(0,0,0,0,0.3)">
-
-			</div>
-			<div class="col-9 p-0 bg-white h-100 d-flex justify-content-center align-items-center">
-
-			</div>
-		{{-- </div> --}}
-	</div>
-</div>
-<div class="h-100 d-flex align-items-center justify-content-center">
-	<div class="row w-100 d-flex justify-content-center" id="form-wrapper">
-		<div class="form-container d-flex flex-row w-75">
-			<div class="img-column col-5 bg-brown h-100 d-flex justify-content-center align-items-center" style="box-shadow:10px 10px rgba(0,0,0,0.3)">
+	<div class="w-100 d-flex align-items-center h-100" id="form-wrapper">
+		<div class="container-form d-flex flex-row col-9">
+			<div class="img-column col-5 bg-brown d-flex justify-content-center align-items-center auth-box-shadow">
 				<img class="mb-4 w-50" src="/images/Naka_Mano_Kohi_yellow.png" alt="kohi-logo">
 			</div>
-			<div class="form-column col-7 p-0 bg-yellow h-100 d-flex justify-content-center align-items-center" style="box-shadow: 10px 10px rgba(0,0,0,0.3)">
+			<div class="form-column col-7 p-0 bg-yellow d-flex justify-content-center align-items-center p-3 auth-box-shadow">
 				<form class="w-75" action="/register" method="post">
 					@csrf
-					<h1 class="h3 mb-3 fw-bold">Buat Akun</h1>
+					<h1 class="mb-3 fw-bold">Buat Akun</h1>
 					<div class="form-floating mb-3">
 						<input type="text" class="form-control rounded-0 border-0 @error('name') is-invalid @enderror" id="floatingInput" placeholder="name@example.com" name="name" value="{{ old('name') }}" autofocus required>
 						<label for="floatingInput">Name</label>
@@ -54,10 +41,9 @@
 					<button class="w-50 button button-brown mb-3 fs-5" type="submit">Shot</button>
 					<p class="fs-5">Akun nya udah ada? <a href="/login" class="text-primary text-decoration-none">Tamping</a></p>
 					{{-- Make your own button effect --}}
-					<p class="text-muted">&copy; Naka Mano Kohi 2022</p>
+					<p class="text-muted m-0">&copy; Naka Mano Kohi 2022</p>
 				</form>
 			</div>
 		</div>
 	</div>
-</div>
 @endsection
