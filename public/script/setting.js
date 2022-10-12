717b528393308a32596d1b1c96868d41b9d0d5d9
ExRyze <vaisyagovinandas@gmail.com>
2022-10-12 14:30:54 +0800

    Update - Setting - Nav Active

--- public/script/setting.js
+++ public/script/setting.js
@@ -1,3 +1,11 @@
 const content = document.querySelector(".setting-content");
-(document.querySelector("#profile")).addEventListener("click", () => {content.innerHTML = `<setting-profile></setting-profile>`});
-(document.querySelector("#notifications")).addEventListener("click", () => {content.innerHTML = `<setting-notifications></setting-notifications>`});
+const profile = document.querySelector("#profile");
+const notifications = document.querySelector("#notifications");
+
+profile.addEventListener("click", () => {content.innerHTML = `<setting-profile></setting-profile>`; active(profile);});
+notifications.addEventListener("click", () => {content.innerHTML = `<setting-notifications></setting-notifications>`;active(notifications);});
+
+function active(element) {
+  (document.querySelector('.active')).classList.remove('active');
+  element.classList.add('active');
+}
