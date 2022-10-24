6427c37c2d3fbb1a68a69a6a3672f599d2cf81f5
ExRyze <vaisyagovinandas@gmail.com>
2022-10-24 14:14:48 +0800

    Minor Update - Member of The Month - User Box

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::get('/{home}', function () {
     'blogs' => Blog::latest()->paginate(4),
     'events' => Events::latest()->get(),
     'date' => Carbon::now()->nthOfMonth(4, Carbon::SATURDAY),
-    'ranking' => DB::table('users')->join('follows', 'users.id', '=', 'follows.user_id')->select('users.*', DB::raw('count(follows.followed_by) as followers'))->groupBy('users.id')->orderBy('followers')->limit('10')->get()->all(),
+    'ranking' => DB::table('users')->leftJoin('follows', 'users.id', '=', 'follows.user_id')->select('users.*', DB::raw('count(follows.followed_by) as followers'))->groupBy('users.id')->orderByDesc('followers')->limit('10')->get()->all(),
     'publicChat' => DB::table('public_chats')->leftJoin('users', 'public_chats.user_id', '=', 'users.id')->select('public_chats.*', 'users.username')->orderByDesc('updated_at')->get()->all(),
     'tz' => getTimezone()
 ]);})->where('home', '(|home)');
