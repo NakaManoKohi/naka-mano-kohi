e7e6a67056d584ac900ef18f29a7aa68f47742b8
ExRyze <vaisyagovinandas@gmail.com>
2022-11-03 14:46:54 +0800

    Update - Profile Activities

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,16 +13,7 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    
-    
-    public function index(User $user)
-    {
-        $data = [
-            'title' => 'Profile',
-            'user' => $user,
-            'followers' => Follows::where([['user_id', $user->id], ['deleted', false]]),
-            'following' => Follows::where([['followed_by', $user->id], ['deleted', false]]),
-        ];
+    function activities($user) {
         function status($rows, $delete, $use, $status) {
             foreach ($rows as $key => $data) {
                 $data->date = $data->$use;
@@ -36,12 +27,24 @@ class ProfileController extends Controller
         }
         $A = status(Follows::where('followed_by', $user->id)->with(['follow', 'follower'])->get()->all(), 'updated_at', 'created_at', 'following');
         $B = status(Follows::where('followed_by', $user->id)->with(['follow', 'follower'])->get()->all(), 'created_at', 'updated_at', 'unfollowing');
-        $data['activities'] = array_merge($A, $B);
-        usort($data['activities'], function($a, $b) {
+        $AB = array_merge($A, $B);
+        usort($AB, function($a, $b) {
             $t1 = strtotime($a->date);
             $t2 = strtotime($b->date);
             return $t2 - $t1;
         });
+        return $AB;
+    }
+    
+    public function index(User $user)
+    {
+        $data = [
+            'title' => 'Profile',
+            'user' => $user,
+            'followers' => Follows::where([['user_id', $user->id], ['deleted', false]]),
+            'following' => Follows::where([['followed_by', $user->id], ['deleted', false]]),
+            'activities' => $this->activities($user)
+        ];
         if (auth()->user() != null) {
             $data['following_user'] = Follows::where([['user_id', $user->id], ['followed_by', auth()->user()->id], ['deleted', false]])->count();
         }
@@ -69,10 +72,9 @@ class ProfileController extends Controller
             'user' => $user,
             'followers' => Follows::where('user_id', $user->id),
             'following' => Follows::where('followed_by', $user->id),
-            'posts' => Post::where('user_id', $user->id)->get()
+            'posts' => Post::where('user_id', $user->id)->get(),
+            'activities' => $this->activities($user)
         ];
-
-        $data['activities'] = DB::table('follows_histories')->join('users as user', 'follows_histories.user_id', '=', 'user.id')->join('users as user_following', 'follows_histories.followed_by', '=', 'user_following.id')->select('follows_histories.*', 'user.username as user', 'user_following.username as user_following')->where('followed_by', $user->id)->orderByDesc('updated_at')->get()->all();
         if (auth()->user() != null) { 
             $data['following_user'] = Follows::where([['user_id', $user->id], ['followed_by', auth()->user()->id]])->count();
         }
